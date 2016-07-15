d90a007d020ce2c02ea5d52dc37458811c6b3acb
Malorie Kramer <mkramer2089@gmail.com>
2016-07-14 22:52:40 -0400

    attempt one at api

--- js/factories/api.js
+++ js/factories/api.js
@@ -1,13 +1,25 @@
-// (function () {
-// 	'use strict';
-// 	angular
-// 		.module('instagram')
-// 		 .factory('API', function($http) {
+(function () {
+	'use strict';
+	angular
+		.module('instagram')
+		 .factory('API', function($http) {
 
-		 	
-// 		 	return {
-// 		 	}
+		 	function getImage(){
+		        var ajaxRequest = $http({
+		          method: 'GET',
+		          headers:
+		          {
+		            X_CSRF_TOKEN: 'malorie',
+		          },
+		          url: 'http://instagramcloneclass.herokuapp.com/images'
+		          });
+
+		        return ajaxRequest;
+   			}
+		 	return {
+		 		getImage:getImage,
+		 	}
 
 		 
-// 		 });
-// })();
+		 });
+})();
